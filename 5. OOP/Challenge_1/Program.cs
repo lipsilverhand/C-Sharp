32322c62a0c333e2312b49477f847cd3e79a964b
lipsilverhand <williamhuynh99@gmail.com>
2023-10-30 18:26:38 -0700

    first commit

---
+++ 5. OOP/Challenge_1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Challenge_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            phone iphone15 = new phone("Apple", "IOS", "09/11/2023");
+            iphone15.introduce();
+            
+        }
+    }
+}
---
+++ 5. OOP/Challenge_1/phone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_1
+    internal class phone
+    {
+        private string _company;
+        private string _operatingSystem;
+        private string _releaseDate; 
+        public phone(string company, string operatingSystem, string releaseDate) {
+            _company = company;
+            _operatingSystem = operatingSystem;
+            _releaseDate = releaseDate;
+        }
+
+        public void introduce()
+        {
+            Console.WriteLine($"It is a product from {_company}, {_operatingSystem}, release on {_releaseDate}");
+        }
+        
+    }
+}
