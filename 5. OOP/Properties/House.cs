41e7570bd4118b059e145a133193366dcc6b58c7
lipsilverhand <williamhuynh99@gmail.com>
2023-11-07 18:37:59 -0800

    first commit

--- 5. OOP/Properties/House.cs
+++ 5. OOP/Properties/House.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace Properties
 {
     internal class House
-    {
+    {   
+        private string _name;
+        private int _price;
+
+        //Constructor
+        public House() {
+            _name = "A";
+            _price = 0;
+        }
+
+        public int Price { 
+            get { return _price; } 
+            set { _price = value; }
+        }
+
+        public string Name {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public void Build()
+        {
+            Console.WriteLine($"{_name} has been deployed.");
+        }
+
+
     }
 }
--- 5. OOP/Properties/Program.cs
+++ 5. OOP/Properties/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Properties
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            House single = new House();
+            single.Name = "Test";
+            single.Build();
+
+            single.Price = 400000;
+            Console.WriteLine($"The price is ${single.Price}.");
+            Console.ReadKey();
+        }
+    }
+}
