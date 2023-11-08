76e880454616579a43638f98463c6cec29ffe69c
lipsilverhand <williamhuynh99@gmail.com>
2023-11-07 19:37:07 -0800

    first commit

---
+++ 5. OOP/Challenge_2/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge_2
+{
+    internal class Book
+    {
+        private string _title;
+        private int _pages;
+
+        public Book() {
+
+        }
+
+        public string Title { 
+            get { return _title; }
+            set
+            {
+                if (value.Equals(""))
+                {
+                    _title = "Unknown";
+                }
+                else
+                {
+                    _title = value;
+                }
+            }
+        }
+
+        public int Pages {
+            get { return _pages; }
+            set { 
+                if (value < 1)
+                {
+                    _pages = 1;
+                } else
+                {
+                    _pages = value;
+                }
+            }
+        }
+    }
+}
---
+++ 5. OOP/Challenge_2/Program.cs
@@ -0,0 +1,20 @@
+﻿using Challenge_2;
+using System;
+
+namespace challenge_2
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+            Book novel = new Book();
+            novel.Title = "";
+            Console.WriteLine(novel.Title);
+
+            novel.Pages = -2;
+            Console.WriteLine(novel.Pages);
+
+            Console.ReadKey();
+
+        }
+    }
+}
