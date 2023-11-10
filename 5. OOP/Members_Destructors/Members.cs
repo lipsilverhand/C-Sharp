22a818d37acf66f7aa512484faa511fe1808bb41
lipsilverhand <williamhuynh99@gmail.com>
2023-11-09 23:27:58 -0800

    first commit

---
+++ 5. OOP/Members_Destructors/Members.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Members_Destructors
+{
+    internal class Members
+    {
+        private string _memberName;
+        private string _jobTitle;
+        private int _salary = 20000;
+
+        public int age;
+        public Members() {
+            age = 24;
+            _memberName = "Lip";
+            _salary = 80000;
+            _jobTitle = "Developer";
+            Console.WriteLine("Object created!");
+        }
+
+        public string jobTitle {  
+            get { return _jobTitle; } 
+            set { _jobTitle = value; } 
+        }
+
+        public void Introducing(bool isFriend)
+        {
+            if (isFriend)
+            {
+                SharingPrivateInfo();
+            } else
+            {
+                Console.WriteLine($"My name is {_memberName}, my job title is {_jobTitle}, and my age is {age}");
+            }
+        }
+
+        private void SharingPrivateInfo ()
+        {
+            Console.WriteLine($"My salary is {_salary}");
+        }
+    }
+}
---
+++ 5. OOP/Members_Destructors/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Members_Destructors
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Members member1 = new Members();
+            member1.Introducing(false);
+            
+
+            Console.ReadKey();
+        }
+    }
+}
