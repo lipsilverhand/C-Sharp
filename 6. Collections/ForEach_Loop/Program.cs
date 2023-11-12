d1718fb8370423d6b3acbb96823264f89e74e171
lipsilverhand <williamhuynh99@gmail.com>
2023-11-11 19:25:30 -0800

    first commit

--- 6. Collections/ForEach_Loop/Program.cs
+++ 6. Collections/ForEach_Loop/Program.cs
@@ -6,7 +6,24 @@ namespace ForEach_Loop
     {
         public static void Main(string[] args)
         {
+            int[] nums = new int[10];
 
+            for(int i = 0; i < 10; i++)
+            {
+                nums[i] = i;
+            }
+            
+            for(int j = 0; j < 10; j++)
+            {
+                Console.WriteLine($"Index {j} = { nums[j]}");
+            }
+
+            int counter = 0;
+            foreach(int k in nums)
+            {
+                Console.WriteLine($"Index {counter} = {k}"); 
+                counter++;
+            }
         }
     }
 }
