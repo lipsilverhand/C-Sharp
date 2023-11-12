e20c84ced8eada771ab992e23c6d5809083598b7
lipsilverhand <williamhuynh99@gmail.com>
2023-11-11 19:30:21 -0800

    first commit

---
+++ 6. Collections/challenge_1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace challenge_1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] friends = new string[] {"A","B","C","D","E"};
+
+            foreach (string friend in friends)
+            {
+                Console.WriteLine($"Greeting {friend}");
+            }
+        }
+    }
+}
