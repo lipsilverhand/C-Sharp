05f9f66c0965a459dad8ed57f52a783174f3bef7
lipsilverhand <williamhuynh99@gmail.com>
2023-11-14 14:02:45 -0800

    first commit

---
+++ 6. Collections/MultiDimension_Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MultiDimension_Array
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[,] maxtrix;
+
+            int[,,] ThreeD;
+
+
+            // 2D array
+            int[,] array2D = new int[,]
+            {
+                {1,2,3},
+                {4,5,6},
+                {6,7,8},
+            };
+
+            Console.WriteLine($"Center value is {array2D[1,1]}"); // [row, index]
+        }
+    }
+}
