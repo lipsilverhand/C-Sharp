7ef5b37bda0fc54e2a9d8cda54b8b4d89d701976
lipsilverhand <williamhuynh99@gmail.com>
2023-11-14 15:05:16 -0800

    first commit

---
+++ 6. Collections/Loop_2DArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Loop_2DArray
+{
+    class Program
+    {   
+        public static void Main(string[] args)
+        {
+            int[,] matrix =
+            {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+            };
+
+            foreach (int num in matrix)
+            {
+                Console.Write(num + " ");
+            }
+
+            Console.WriteLine("--------------");
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                }
+            }
+        }
+    }
+}
