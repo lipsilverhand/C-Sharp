4522b628f96271392b2b75229a348467b5df8a88
lipsilverhand <williamhuynh99@gmail.com>
2023-11-18 16:20:13 -0800

    first commit

---
+++ 6. Collections/challenge_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace challenge_3
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[][] FriendArrays = new string[][]
+            {
+                new string[] {"Kevin","Stacy"},
+                new string[] {"Bob","Tom","Kathy"},
+                new string[] {"John","Alan","Tim","Will","Stephanie"}
+            };
+
+            for (int i = 0; i < FriendArrays.Length; i++)
+            {
+                Console.WriteLine($"Family {i + 1}, introducing: ");
+                for (int j = 0; j < FriendArrays[i].Length; j++)
+                {
+                    Console.WriteLine(FriendArrays[i][j]);
+                }
+            }
+        }
+    }
+}
