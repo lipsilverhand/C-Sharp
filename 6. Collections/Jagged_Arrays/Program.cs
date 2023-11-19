659b528ed74f8c776629ebdca96d8750ce81b2dc
lipsilverhand <williamhuynh99@gmail.com>
2023-11-18 16:12:18 -0800

    first commit

---
+++ 6. Collections/Jagged_Arrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Jagged_Arrays
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[][]
+            {
+                new int[] { 1, 2, 3, 4, 5 },
+                new int[] { 5, 10, 15, 20, 25, 30 }
+            };
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                Console.Write($"Element {i}");
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.WriteLine(jaggedArray[i][j]);
+                }
+            }
+        }
+    }
+}
---
+++ 6. Collections/TicTacToe/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TicTocToe
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Table();
+        }
+
+
+        public static void Table()
+        {
+            Console.WriteLine($"| {1} | | {2} | | {3} | ");
+            Console.WriteLine("-----------------");
+            Console.WriteLine($"| {4} | | {5} | | {6} | ");
+            Console.WriteLine("-----------------");
+            Console.WriteLine($"| {7} | | {8} | | {9} | ");
+
+        }
+    }
+}
