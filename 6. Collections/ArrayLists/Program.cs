784e19366f55ac91d858a77292347c866c3c51a7
lipsilverhand <williamhuynh99@gmail.com>
2023-11-20 19:29:06 -0800

    first commit

---
+++ 6. Collections/ArrayLists/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace ArrayListC
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ArrayList myArrayList = new ArrayList();
+            myArrayList.Add(1);
+            myArrayList.Add("Bello");
+            myArrayList.Add(2.4);
+            double sum = 0;
+
+            foreach (object i in myArrayList)
+            {
+                if(i is int)
+                {
+                    sum += Convert.ToDouble(i);
+                } else if (i is double) {
+                    sum += (double)i;
+                } else if (i is string)
+                {
+                    Console.WriteLine(i);
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
