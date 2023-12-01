04d479921b60ea201e4b1b588f611b356016870c
lipsilverhand <williamhuynh99@gmail.com>
2023-12-01 12:32:26 -0800

    first commit

---
+++ 6. Collections/challenge_5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+
+namespace challenge_5
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Hashtable table = new Hashtable();
+
+            Student[] studentList = new Student[6];
+
+            studentList[0] = new Student(1, "Lip", 24);
+            studentList[1] = new Student(2, "Pisua", 18);
+            studentList[2] = new Student(3, "Alpha", 30);
+            studentList[3] = new Student(4, "Beta", 12);
+            studentList[4] = new Student(1, "Lip", 24);
+            studentList[5] = new Student(6, "Pisua", 24);
+
+
+            foreach (Student s in studentList) 
+            {
+                if (!table.Contains(s.ID))
+                {
+                    table.Add(s.ID, s);
+                    Console.WriteLine($"Student with ID {s.ID} was added!");
+                } else
+                {
+                    Console.WriteLine($"Sorry, a student with a same ID already existed {s.ID}.");
+                }
+
+            }
+
+        }
+    }
+}
---
+++ 6. Collections/challenge_5/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenge_5
+{
+    internal class Student
+    {
+        public int ID { get; set; }
+        public string NAME { get; set; }
+        public int AGE { get; set; }
+
+        //Constructor
+        public Student(int id, string name, int age)
+        {
+            this.ID = id;
+            this.NAME = name;
+            this.AGE = age;
+        }
+    }
+}
