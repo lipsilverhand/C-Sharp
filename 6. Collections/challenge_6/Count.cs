5eb94fdaf0303a9bfcff1d55d735a6601f3323f7
lipsilverhand <williamhuynh99@gmail.com>
2023-12-02 15:15:43 -0800

    first commit

---
+++ 6. Collections/challenge_6/Count.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace challenge_6
+{
+    internal class Count
+    {
+        public int NUMBER {  get; set; }
+        public string WORD { get; set; }
+
+
+        // Constructor
+        public Count(int number, string word) 
+        {
+            this.NUMBER = number;
+            this.WORD = word;
+        }
+    }
+}
---
+++ 6. Collections/challenge_6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+namespace challenge_6
+{
+    class Program
+    {   
+        public static void Main(string[] args)
+        {
+            Count[] setList = {
+            new Count(1,"one"),
+            new Count(2,"two"),
+            new Count(3,"three"),
+            new Count(4,"four"),
+            new Count(5, "five")
+        };
+
+            Dictionary<int, Count> numberDict = new Dictionary<int, Count>();
+            foreach (Count x in setList)
+            {
+                numberDict.Add(x.NUMBER, x);
+            }
+
+            int[] key = {1,2,3,4,5};
+
+            foreach (int i in key) 
+            {
+                if (numberDict.ContainsKey(i))
+                {
+                    Console.WriteLine($"Return {numberDict[i].NUMBER}, \"{numberDict[i].WORD}\"");
+                } else
+                {
+                    Console.WriteLine("Nope...");
+                }
+            }
+        }
+        
+
+    }
+}
