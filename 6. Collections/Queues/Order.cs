947aba2819da706701597e53b3b85b5365d1f7fb
lipsilverhand <williamhuynh99@gmail.com>
2023-12-03 14:42:15 -0800

    first commit

---
+++ 6. Collections/Queues/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    internal class Order
+    {
+        public int ID {  get; set; }
+        public int ORDERQUANLITY { get; set; }
+        public Order(int id, int orderQuanlity) 
+        {
+            this.ID = id;
+            this.ORDERQUANLITY = orderQuanlity;
+        }
+
+        public void ProcessOrder () 
+        {
+            Console.WriteLine($"Order {ID} processed!");
+        }
+    }
+}
---
+++ 6. Collections/Queues/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+
+namespace Queues
+{
+    class Program
+    {
+        public static void Main(string[] args) 
+        {
+            Queue<Order> orderQueue = new Queue<Order>();
+            foreach (Order o in ReceiveOrder1()) 
+            {
+                orderQueue.Enqueue(o);
+            }
+
+            foreach (Order o in ReceiveOrder2())
+            {
+                orderQueue.Enqueue(o);
+            }
+
+            while (orderQueue.Count > 0)
+            {
+                Order currentOrder = orderQueue.Dequeue();
+                currentOrder.ProcessOrder();
+            }
+        }
+
+        public static Order[] ReceiveOrder1() 
+        {
+            Order[] orders = new Order[]
+            {
+                new Order(1,5),
+                new Order(2, 4),
+                new Order(5, 8),
+            };
+            return orders;
+        }
+
+        public static Order[] ReceiveOrder2()
+        {
+            Order[] orders = new Order[]
+            {
+                new Order(3,5),
+                new Order(4, 4),
+                new Order(6, 8),
+            };
+            return orders;
+        }
+
+    }
+}
