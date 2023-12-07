70836f8b95d067d4405d04da43186040ec9b00b8
lipsilverhand <williamhuynh99@gmail.com>
2023-12-06 20:40:56 -0800

    first commit

---
+++ 8. OOP Inheritance/Simple_Inheritance/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Simple_Inheritance
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ 8. OOP Inheritance/Simple_Inheritance/Radio.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Inheritance
+{
+    internal class Radio
+    {
+        public bool IsOn {  get; set; }
+        public string BRAND { get; set; }
+        public Radio(bool ison, string brand) 
+        {
+            this.IsOn = ison;
+            this.BRAND = brand;
+        }
+
+        public void SwitchOn()
+        {
+            IsOn = true;
+        }
+
+        public void SwitchOff()
+        {
+            IsOn &= false;
+        }
+
+        public void ListenRadio() 
+        {
+            if (IsOn)
+            {
+                Console.WriteLine("Listening to the radio");
+            } else
+            {
+                Console.WriteLine("Radio is off!");
+            }
+        }
+    }
+}
