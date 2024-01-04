e317bcc937b6d2967f9c9fbac12f3ca2cb5bd1b5
lipsilverhand <williamhuynh99@gmail.com>
2024-01-04 14:08:19 -0800

    first commit

---
+++ 12. WPF/ToDoList Desktop App/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToDoList_Desktop_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void AddToDo_Button(object sender, RoutedEventArgs e)
+        {
+            string todoText = textbox.Text;
+
+            if(!string.IsNullOrEmpty(todoText))
+            {
+                TextBlock todoItem = new TextBlock
+                {
+                    Text = todoText,
+                    Margin = new Thickness(10),
+                    Foreground = new SolidColorBrush(Colors.White),
+                };
+                todolist.Children.Add(todoItem);
+                textbox.Clear();
+            }
+        }
+    }
+}
