cf58a225f22b4d860dbbf43048ddc2a1a115c280
lipsilverhand <williamhuynh99@gmail.com>
2024-01-12 15:57:01 -0800

    first commit

---
+++ 12. WPF/DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using DataBinding.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Person person = new Person
+        {
+            NAME = "Lip",
+            AGE = 30
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = person;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string personData = person.NAME + " is " + person.AGE + " years old.";
+            MessageBox.Show(personData);
+        }
+    }
+}
---
+++ 12. WPF/ListBox/Data/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListBox.Data
+{
+    public class Person
+    {
+        public string NAME { get; set; }
+        public int AGE { get; set; }
+    }
+}
