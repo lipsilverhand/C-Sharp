f76c4781e668f2258b6ef72479ae91a90a04d82d
lipsilverhand <williamhuynh99@gmail.com>
2024-01-12 16:50:02 -0800

    first commit

---
+++ 12. WPF/LoginUser/LoginView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LoginUser
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        private void Password_Button(object sender, RoutedEventArgs e)
+        {
+            string passwordInput = PasswordBox.Password;
+            string ePassword = Environment.GetEnvironmentVariable("UserLogin");
+                
+            if(ePassword != null)
+            {
+                if (ePassword == passwordInput)
+                {
+                    MessageBox.Show("Enter correct password");
+
+                } else
+                {
+                    MessageBox.Show("Wrong password, try again");
+
+                }
+            } else
+            {
+                MessageBox.Show("Environment variable not found!");
+            }
+        }
+    }
+}
